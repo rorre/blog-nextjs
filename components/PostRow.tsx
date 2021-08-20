fff58b9180b65120c555255f1e5256c89288e78f
Rendy Arya Kemal <rendyarya22@gmail.com>
2021-08-20 15:47:46 +0700

    Add key to PostRow iterator

--- components/PostRow.tsx
+++ components/PostRow.tsx
@@ -11,7 +11,15 @@ export default function PostRow({ rowKey, posts }: PostRowProps) {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 space-y-4 md:space-y-0 md:space-x-4 py-2">
             {posts.map((post, idx) => {
-                return <Card id={post.Slug} title={post.Title} preview={post.previewParagraph} datetime={post.Date} />
+                return (
+                    <Card
+                        id={post.Slug}
+                        title={post.Title}
+                        key={'post-' + idx}
+                        preview={post.previewParagraph}
+                        datetime={post.Date}
+                    />
+                )
             })}
         </div>
     )
