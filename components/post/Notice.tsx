fec5ae7cc7901d0a896760cdb9343f672c79605c
Rendy Arya Kemal <rendyarya22@gmail.com>
2021-08-20 15:43:52 +0700

    Reformat Notice

--- components/post/Notice.tsx
+++ components/post/Notice.tsx
@@ -1,37 +1,41 @@
 import { IconProp, library, SizeProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
-import React from 'react';
+import React from 'react'
 
-library.add(faExclamationTriangle);
+library.add(faExclamationTriangle)
 
 interface NoticeProps extends React.HTMLProps<React.ReactElement> {
-    faIcon: IconProp;
-    faIconSize: SizeProp;
-    noticeType: string;
-    header: string;
-    content: string;
+    faIcon: IconProp
+    faIconSize: SizeProp
+    noticeType: string
+    header: string
+    content: string
 }
 
-export default function Notice({ faIcon, faIconSize = "3x", noticeType, header, content, children }: NoticeProps) {
-    let colorClass = ""
+export default function Notice({ faIcon, faIconSize = '3x', noticeType, header, content, children }: NoticeProps) {
+    let colorClass = ''
 
     switch (noticeType) {
-        case "warning":
-            colorClass = "border-yellow-400"
+        case 'warning':
+            colorClass = 'border-yellow-400'
             break
-        case "info":
-            colorClass = "border-blue-500"
+        case 'info':
+            colorClass = 'border-blue-500'
             break
-        case "error":
-            colorClass = "border-red-500"
+        case 'error':
+            colorClass = 'border-red-500'
             break
         default:
             break
     }
 
     return (
-        <div className={`flex items-center justify-center p-4 rounded-lg shadow-md bg-white border ${colorClass} space-x-4`}>
+        <div
+            className={`flex flex-col md:flex-row items-center justify-center
+                        p-4 rounded-lg shadow-md bg-white border ${colorClass}
+                        space-x-4 space-y-4 md:space-y-0`}
+        >
             {faIcon && <FontAwesomeIcon icon={faIcon} size={faIconSize} />}
 
             <div className="text-left">
@@ -39,6 +43,6 @@ export default function Notice({ faIcon, faIconSize = "3x", noticeType, header, 
                 {header && <h4 className="font-bold text-md">{header}</h4>}
                 {content && <p>{content}</p>}
             </div>
-        </div >
+        </div>
     )
 }
